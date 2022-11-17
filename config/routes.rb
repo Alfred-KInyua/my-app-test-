172a7286d7be4ba75ea3f1fa2ded6ec4222443ac
Kinyua <alfkinyua@gmail.com>
2022-11-17 14:24:40 +0300

    Add models in rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :articles
   root 'welcome#home'
   get 'index', to: 'about#index'
   get 'contact/reach', to: 'contact#reach'
